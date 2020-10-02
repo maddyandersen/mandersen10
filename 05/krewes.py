5db934549b9b9d2e3d8055cc4615545712f86ca9
maddyandersen <mandersen10@stuy.edu>
2020-10-02 00:57:22 -0400

    k05: reverting back to original version

--- 05/krewes.py
+++ 05/krewes.py
@@ -1,10 +1,9 @@
-# Triple M: Maddy Andersen, Mary Shang, Renee Mui
+# Maddy Andersen (with Saqif Abedin and Ian Chen-Adamczyk)
 # SoftDev
-# K06 -- Learnination Through Amalgamation: Re-factoring our K05 code with our new teammates 
-# 2020-10-01
+# K05 -- Teamwork, but Better This Time: prints a random name from the team selected by the user
+# 2020-09-30
 
-# imports random module (necessary to use random.choice() later in the code)
-import random 
+import random
 
 KREWES = {
     'orpheus': ['ERIC', 'SAUVE', 'JONATHAN', 'PAK', 'LIAM', 'WINNIE', 'KELLY', 'JEFFREY', 'KARL', 'ISHITA', 'VICTORIA', 'BENJAMIN', 'ARIB', 'AMELIA', 'CONSTANCE', 'IAN'],
@@ -12,13 +11,8 @@ KREWES = {
     'endymion': ['JASON', 'DEAN', 'MADDY', 'SAQIF', 'CINDY', 'YI LING', 'RUOSHUI', 'FB', 'MATTHEW', 'MAY', 'ERIN', 'MEIRU']
 }
 
-# asks user for team name and converts it to all lower case
 team = input("Pick a team from orpheus, rex, or endymion: ").lower()
-
-# if the user inputs a name that doesn't match the team name, prompts user to enter a correct team name
 while team not in KREWES:
     team = input("Please enter either orpheus, rex, or endymion: ").lower()
 
-# prints a random student name from the chosen team
-# uses random.choice()
 print(random.choice(KREWES[team]))
